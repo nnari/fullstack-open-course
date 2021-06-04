3ae1c0f58314143134298aa58a87588ce9bf87e4
Tatu Pesonen <pannariiz@gmail.com>
2021-06-04 17:40:12 +0300

    Cherrypick old file I accidentally replaced

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,27 +1,44 @@
-import { useState } from "react";
-
 const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  const handleFeedbackClick = (keyName) =>
-    setFeedback((prev) => ({
-      ...prev,
-      [keyName]: prev[keyName] + 1,
-    }));
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <>
-      <button onClick={() => handleFeedbackClick("good")}>Good</button>
-      <button onClick={() => handleFeedbackClick("neutral")}>Neutral</button>
-      <button onClick={() => handleFeedbackClick("bad")}>Bad</button>
-      <p>
-        {feedback.bad} {feedback.good}
-      </p>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 };
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Content = ({ parts }) => parts.map((p) => <Part part={p} />);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((acc, cur) => (acc += cur.exercises), 0)}
+  </p>
+);
 
 export default App;
