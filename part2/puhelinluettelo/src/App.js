74f9d2b8650856c73ad4ead4dec4787dc0931cb1
Tatu Pesonen <pannariiz@gmail.com>
2021-06-06 18:25:41 +0300

    Finish part 2 2.1 and refactor

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -3,13 +3,10 @@ const App = () => {
   const [people, setPeople] = useState([
     { name: "Tatu Pesonen", number: "045 12312321312" },
   ]);
-
   const [filter, setFilter] = useState("");
+  const [newPerson, setNewPerson] = useState({});
 
   const handleFilterChange = (event) => setFilter(event.target.value);
-
-  // State container for the new input
-  const [newPerson, setNewPerson] = useState({});
 
   /*
   Generic over field that you want to set. 
@@ -21,10 +18,13 @@ const App = () => {
       ...prev,
       [field]: event.target.value,
     }));
+
   const handlePeopleAdd = (event) => {
     event.preventDefault();
     // Check whether the people array already contains user with the same name
-    if (people.some((p) => p.name === newPerson.name)) {
+    if (
+      people.some((p) => p.name.toLowerCase() === newPerson.name.toLowerCase())
+    ) {
       alert(`${newPerson.name} is already in the phonebook.`);
       return;
     }
@@ -34,48 +34,67 @@ const App = () => {
     setNewPerson({ name: "", number: "" });
   };
 
-  // Lastly create the filter
   const peopleToShow = filter
     ? people.filter((p) => p.name.toLowerCase().includes(filter.toLowerCase()))
     : people;
 
-  // Prettier keeps adding these {" "} for spaces in JSX...
-
   return (
     <>
-      <div>
-        Filter selection: <input value={filter} onChange={handleFilterChange} />
-      </div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name:{" "}
-          <input
-            value={newPerson.name}
-            onChange={(event) => handleNewPersonChange(event, "name")}
-          />
-        </div>
-        <div>
-          number:{" "}
-          <input
-            value={newPerson.number}
-            onChange={(event) => handleNewPersonChange(event, "number")}
-          />
-        </div>
-        <div>
-          <button onClick={handlePeopleAdd}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {peopleToShow.map((p) => (
-          <p key={p.name}>
-            {p.name} {p.number}
-          </p>
-        ))}
-      </div>
+      <h1>Phonebook</h1>
+      <FilterForm filter={filter} onChangeHandler={handleFilterChange} />
+      <AddPersonForm
+        addHandler={handlePeopleAdd}
+        changeHandler={handleNewPersonChange}
+        person={newPerson}
+      />
+      <PhoneBook people={peopleToShow} />
     </>
   );
 };
+
+const FilterForm = ({ filter, onChangeHandler }) => (
+  <>
+    <h2>Filter numbers</h2>
+    Filter selection: <input value={filter} onChange={onChangeHandler} />
+  </>
+);
+
+const PhoneBook = ({ people }) => (
+  <>
+    <h2>Numbers</h2>
+    <div>
+      {people.map((p) => (
+        <p key={p.name}>
+          {p.name} {p.number}
+        </p>
+      ))}
+    </div>
+  </>
+);
+
+const AddPersonForm = ({ addHandler, changeHandler, person }) => (
+  <>
+    <h2>Add new person</h2>
+    <form>
+      <div>
+        name:{" "}
+        <input
+          value={person.name}
+          onChange={(event) => changeHandler(event, "name")}
+        />
+      </div>
+      <div>
+        number:{" "}
+        <input
+          value={person.number}
+          onChange={(event) => changeHandler(event, "number")}
+        />
+      </div>
+      <div>
+        <button onClick={addHandler}>add</button>
+      </div>
+    </form>
+  </>
+);
 
 export default App;
